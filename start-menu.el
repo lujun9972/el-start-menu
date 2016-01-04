209745fb37d2fea03e64537e22514f606486a24c
darksun <lujun9972@sina.com>
2016-01-04 09:19:42 +0800

    add enable and disable function

--- start-menu.el
+++ start-menu.el
@@ -36,9 +36,10 @@
 
 ;; Quick start:
 
-;; If you are using debian linux or ubuntu linux just (require 'start-menu)
-;; Otherwise, you should config start-menu-menu-conf first then (require 'start-menu)
+;; If you are using debian linux or ubuntu linux just M-x start-menu-enable
+;; Otherwise, you should config start-menu-menu-conf first then M-x start-menu-enable
 ;; For Windows user: Sine the path to program often contains space, so may be you use "" to quote the path
+;; If you don't need Start menu any more,just M-x start-menu-disable
 
 ;;; Code:
 (require 'cl-lib)
